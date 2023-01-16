7df189b51b731245207909f8b5e96f5acf9c9ef5
Alef Murick <alefmurick@hotmail.com>
2023-01-16 05:14:51 -0300

    correção lint

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,15 +1,15 @@
 db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
 
 db.produtos.updateMany({
-  tags: { $all: ["bovino"] }
+  tags: { $all: ["bovino"] },
 }, {
-  $inc: { avaliacao: 5 }
+  $inc: { avaliacao: 5 },
 });
 
 db.produtos.updateMany({
-  tags: { $all: ["ave"] }
+  tags: { $all: ["ave"] },
 }, {
-  $inc: { avaliacao: 3 }
+  $inc: { avaliacao: 3 },
 });
 
-db.produtos.find({}, {nome: 1, avaliacao: 1, _id: 0});
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,4 +1,4 @@
 db.produtos.updateOne({ nome: { $in: ["Cheddar McMelt"] } },
-  { $pop: {ingredientes: 1} })
+  { $pop: { ingredientes: 1 } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
